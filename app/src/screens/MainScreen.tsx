4c41ef467aa7fe187a1f4bac57b696e14396ee55
Andrei-Albert Gherasim <gherasim_albert@yahoo.com>
2023-05-21 19:35:59 +0300

    refactored unnecessary code

--- app/src/screens/MainScreen.tsx
+++ app/src/screens/MainScreen.tsx
@@ -1,35 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-  redirect
-} from 'react-router-dom';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
-import { Formik } from 'formik';
-import axios from 'axios';
-import axiosClient from '../services/axiosClient';
+import React, { useEffect } from 'react';
 import StudentScreen from './StudentScreen';
 import { useNavigate } from 'react-router-dom';
 import TeacherScreen from './TeacherScreen';
 import Navbar from '../components/Navbar';
-import Alert from 'react-bootstrap/Alert';
-import Modal from 'react-bootstrap/Modal';
 
 
 function MainScreen() {
-  const [validated, setValidated] = useState(false);
   const navigate = useNavigate();
-  const [user, setUser] =
-    useState<{
-      studentDetails: ({} | null),
-      teacherDetails: ({ id: number } | null)
-    } | null>(null);
 
   useEffect(() => {
     if (
